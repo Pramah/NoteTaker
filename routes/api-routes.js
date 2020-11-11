abbc943e49de2f4be18af5f25c21c0bfecc84b31
prasanna <bagilip@gmail.com>
2020-11-10 16:26:32 -0800

    adding more files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,13 @@
 const db = require('../db/db.json');
 const fs = require("fs");
-const uuid = require("uuid/v4");
+const uuid = require("uuid");
 
-module.exports = function(app) {
-  app.get("/api/notes", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
     res.send(db);
   });
 
-  app.post("/api/notes", function(req, res) {
+  app.post("/api/notes", function (req, res) {
 
     let noteId = uuid();
     let newNote = {
